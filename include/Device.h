c9e8db023429f75dff85febfcccf06e3e1c00386
busy <1324789783@qq.com>
2023-09-23 00:14:39 +0800

    [feature] 添加项目需求文档

---
+++ include/Device.h
@@ -0,0 +1,15 @@
+#ifndef __BUSY_DEVICE_H__
+#define __BUSY_DEVICE_H__
+
+#include <iostream>
+#include <cstring>
+
+class IDevice
+{
+public:
+    IDevice();
+    ~IDevice();
+
+};
+
+#endif // __BUSY_DEVICE_H__
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Hello VirtualDevice!" << std::endl;
+    return 0;
+}
